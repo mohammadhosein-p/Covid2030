26c367f6981e2543663fa0ef74db1409cbad2b9d
Amirreza <amir.samari1383@gmail.com>
2024-01-20 15:50:44 +0330

    add ingame_menu and fix bugs like newgame levelfinish() health and add needed system(cls) and sleep

--- main.cpp
+++ main.cpp
@@ -13,10 +13,10 @@ int kheshab=0;
 int health=3 ;
 int credit=0;
 int game_round=0;
-int final_level=3;
+int final_level=1;
 int vaccineCollected = 0;
 char order[1];
-int arr[15][15] = {0}; 
+int arr[15][15] = {0};
 bool is_sound_on = false;
 HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
@@ -77,6 +77,7 @@ void reset_color() {
 }
 
 void main_menu();
+void ingame_menu();
 void start();
 void print_health_status();
 void print_ammo_status();
@@ -109,11 +110,10 @@ int main() {
 
 	main_menu();
 	system("cls");
-	start();
 	print_play_ground();
 
 	while(true) {
-		cin >> order;
+		cin >> order[0];
 		game_round++;
 		zombieMoveCounter++;
 		player_function(order[0]);
@@ -121,11 +121,20 @@ int main() {
 		reload(order[0]);
 		upgrade(order[0]);
 		levelFinish();
-		if (order[0] == 'e' or order[0] == 'E')
+		if (order[0] == 'e' or order[0] == 'E') {
+			system("cls");
 			main_menu();
+
+		}
+
+
 		if (zombieMoveCounter % 2 ==0)
 			zombi_Movment();
 		death();
+		if (order[0]=='m' or order[0]=='M') {
+			system("cls");
+			ingame_menu();
+		}
 		system("cls");
 		print_play_ground();
 	}
@@ -147,6 +156,17 @@ void main_menu() {
 	system("cls");
 	switch (order[0]) {
 		case '1': // NEW GAME
+			final_level = 1;
+			credit = 0 ;
+			game_round=0;
+			kill = 0;
+			vaccineCollected = 0;
+			tir=3;
+			kheshab=0;
+			health=3 ;
+			maxBullet = 3;
+			range=5;
+			start();
 			system("cls");
 			break;
 
@@ -202,6 +222,7 @@ void main_menu() {
 			system("cls");
 			if (order[0] == 'y') {
 				cout << "Good Bye!";
+				sleep(2.5);
 				exit(0);
 			}
 
@@ -219,11 +240,92 @@ void main_menu() {
 	return;
 }
 
+void ingame_menu() {
+	cout << "0 - Return to game\t1 - New Game\t2 - Settings\t3 - Exit\n";
+	cin >> order[0];
+	system("cls");
+	if ( order[0] != '0' && order[0]!= '1' && order[0]!= '2' && order[0]!='3') {
+		while (true) {
+			cout << "Please just enter the numbers in the menu:\n";
+			cin >> order[0];
+			system("cls");
+			if (order[0] == '0' || order[0]== '1' || order[0]== '2' || order[0]=='3' )
+				break;
+		}
+	}
+	switch (order[0]) {
+		case '0': //return
+			game_round--;
+			system("cls");
+			break;
+
+		case '1': // NEW GAME
+			final_level = 1;
+			credit = 0 ;
+			game_round = 0;
+			kill = 0;
+			vaccineCollected = 0;
+			tir=3;
+			kheshab=0;
+			health=3 ;
+			maxBullet = 3;
+			range=5;
+			start();
+			system("cls");
+			break;
+
+		case '2':
+			cout << "Final level is: " << final_level << endl;
+			if (is_sound_on == false) {
+				cout << "Sound is mute!\nDo you want to unmute the sound? (y : YES | n : NO)\n";
+				cin >> order[0];
+				system("cls");
+				if (order[0] == 'n') {
+					//	return main_menu();
+					return;
+				} else if (order[0] == 'y') {
+					is_sound_on = true;
+					//	return main_menu();
+					return;
+				} else {
+					//	return main_menu();
+					return;
+				}
+			} else if (is_sound_on == true) {
+				cout << "Sound is not mute!!\nDo you want to mute the sound? (y : YES | n : NO)\n";
+				cin >> order[0];
+				system("cls");
+				if (order[0] == 'n') {
+					//return main_menu();
+					return;
+				} else if (order[0] == 'y') {
+					is_sound_on = false;
+					//return main_menu();
+					return;
+				} else {
+					//	return main_menu();
+					return;
+				}
+			}
+
+			break;
+
+		case '3':
+			main_menu();
+
+			break;
+
+	}
+	return;
+
+}
+
 void start() {
-	for (int i = 1; i<14; i++)
-		for (int j=1; j<14; j++)
+	for (int i = 0; i<=14; i++)
+		for (int j=0; j<=14; j++)
 			arr[i][j] = 0;
-	arr[0][0] = 1; arr[14][14]=2; // player num = 1 or 6, destination num = 2
+	arr[0][0] = 1;
+	arr[14][14]=2; // player num = 1 or 6, destination num = 2
 	for (int index = 0; index<final_level; index++) { //select zambie location
 		int i = rand()%12+3, j = rand()%12+3;
 		if (arr[i][j] == 0)
@@ -235,14 +337,14 @@ void start() {
 		int i = rand()%15, j = rand()%15;
 		if (arr[i][j] == 0)
 			arr[i][j] = 4; //vaccine num = 4
-		else 
+		else
 			index--;
 	}
 	for (int index = 0 ; index<(final_level+1)/2; index++) {
 		int i = rand()%15, j = rand()%15;
 		if (arr[i][j] == 0)
 			arr[i][j] = 3; //ammo num = 3
-		else 
+		else
 			index--;
 	}
 }
@@ -276,19 +378,19 @@ void print_play_ground() {
 				green();
 				cout << "P";
 			}
-			if (arr[i-1][j-2]==2){
+			if (arr[i-1][j-2]==2) {
 				cyan();
 				cout << "D";
 			}
-			if (arr[i-1][j-2]==5||arr[i-1][j-2]>6){
+			if (arr[i-1][j-2]==5||arr[i-1][j-2]>6) {
 				magenta();
-				cout << "z";
-			}	
+				cout << "Z";
+			}
 			if (arr[i-1][j-2]==4) {
 				yellow();
 				cout << "V";
 			}
-			if (arr[i-1][j-2]==3){
+			if (arr[i-1][j-2]==3) {
 				blue_bold();
 				cout << "A";
 			}
@@ -299,267 +401,229 @@ void print_play_ground() {
 	}
 	cout << "-----------------" << endl;
 }
-void player_function(char x)
-{
+void player_function(char x) {
 	int i,j;
-	for(int s=0;s<15;s++)
-		   for(int z=0;z<15;z++)
-		   	if(arr[s][z]==1||arr[s][z]==6)
-		   	{
-		   		i=s;
-		   		j=z;
+	for(int s=0; s<15; s++)
+		for(int z=0; z<15; z++)
+			if(arr[s][z]==1||arr[s][z]==6) {
+				i=s;
+				j=z;
 			}
-	if(x=='w'||x=='W')
-	{
-		if(i-1>-1)
-		{
-	     	if(arr[i-1][j]==4)
-		    {
-			   vaccineCollected++;
-			   credit += final_level + 1;
-			   arr[i-1][j]=1;
-		    }
-		    else if(arr[i-1][j]==3)
-		    {
-			   kheshab++;
-			   arr[i-1][j]=1;
-	     	}
-		    else if(arr[i-1][j]==5)
-		    {
-		    	arr[i-1][j]=6;
-			}
-			else if(arr[i-1][j]==0)
-		    {
-		    	arr[i-1][j]=1;
+	if(x=='w'||x=='W') {
+		if(i-1>-1) {
+			if(arr[i-1][j]==4) {
+				vaccineCollected++;
+				credit += final_level + 1;
+				arr[i-1][j]=1;
+			} else if(arr[i-1][j]==3) {
+				kheshab++;
+				arr[i-1][j]=1;
+			} else if(arr[i-1][j]==5) {
+				arr[i-1][j]=6;
+			} else if(arr[i-1][j]==0) {
+				arr[i-1][j]=1;
 			}
 			arr[i][j]--;
-	   }
-	}
-	else if(x=='s'||x=='S')
-	{
-		if(i+1<15)
-		{
-		    if(arr[i+1][j]==4)
-		    {
-			   vaccineCollected++;
-			   credit += final_level + 1;
-			   arr[i+1][j]=1;
-		    }
-		    else if(arr[i+1][j]==3)
-		    {
-			   kheshab++;
-			   arr[i+1][j]=1;
-	     	}
-		    else if(arr[i+1][j]==5)
-		    {
-		    	arr[i+1][j]=6;
-			}
-			else if(arr[i+1][j]==0)
-		    {
-		    	arr[i+1][j]=1;
-			}
-			else if (arr[i+1][j]==2)
-			{
+		}
+	} else if(x=='s'||x=='S') {
+		if(i+1<15) {
+			if(arr[i+1][j]==4) {
+				vaccineCollected++;
+				credit += final_level + 1;
+				arr[i+1][j]=1;
+			} else if(arr[i+1][j]==3) {
+				kheshab++;
+				arr[i+1][j]=1;
+			} else if(arr[i+1][j]==5) {
+				arr[i+1][j]=6;
+			} else if(arr[i+1][j]==0) {
+				arr[i+1][j]=1;
+			} else if (arr[i+1][j]==2) {
 				arr[i+1][j]=1;
 			}
 			arr[i][j]--;
-	   }
-	}
-	else if(x=='d'||x=='D')
-	{
-		if(j+1<15)
-		{
-		   if(arr[i][j+1]==4)
-		    {
-			   vaccineCollected++;
-			   credit += final_level + 1;
-			   arr[i][j+1]=1;
-		    }
-		    else if(arr[i][j+1]==3)
-		    {
-			   kheshab++;
-			   arr[i][j+1]=1;
-	     	}
-		    else if(arr[i][j+1]==5)
-		    {
-		    	arr[i][j+1]=6;
-			}
-			else if(arr[i][j+1]==0)
-		    {
-		    	arr[i][j+1]=1;
-			}
-			else if(arr[i][j+1]==2)
-		    {
-		    	arr[i][j+1]=1;
-			}
-			arr[i][j]--;
-	    }
-	}
-	else if(x=='a'||x=='A')
-	{
-	   if(j-1>-1)
-	    {
-		   if(arr[i][j-1]==4)
-		    {
-			   vaccineCollected++;
-			   credit += final_level + 1;
-			   arr[i][j-1]=1;
-		    }
-		    else if(arr[i][j-1]==3)
-		    {
-			   kheshab++;
-			   arr[i][j-1]=1;
-	     	}
-		    else if(arr[i][j-1]==5)
-		    {
-		    	arr[i][j-1]=6;
-			}
-			else if(arr[i][j-1]==0)
-		    {
-		    	arr[i][j-1]=1;
+		}
+	} else if(x=='d'||x=='D') {
+		if(j+1<15) {
+			if(arr[i][j+1]==4) {
+				vaccineCollected++;
+				credit += final_level + 1;
+				arr[i][j+1]=1;
+			} else if(arr[i][j+1]==3) {
+				kheshab++;
+				arr[i][j+1]=1;
+			} else if(arr[i][j+1]==5) {
+				arr[i][j+1]=6;
+			} else if(arr[i][j+1]==0) {
+				arr[i][j+1]=1;
+			} else if(arr[i][j+1]==2) {
+				arr[i][j+1]=1;
 			}
 			arr[i][j]--;
 		}
-			
+	} else if(x=='a'||x=='A') {
+		if(j-1>-1) {
+			if(arr[i][j-1]==4) {
+				vaccineCollected++;
+				credit += final_level + 1;
+				arr[i][j-1]=1;
+			} else if(arr[i][j-1]==3) {
+				kheshab++;
+				arr[i][j-1]=1;
+			} else if(arr[i][j-1]==5) {
+				arr[i][j-1]=6;
+			} else if(arr[i][j-1]==0) {
+				arr[i][j-1]=1;
+			}
+			arr[i][j]--;
+		}
+
 	}
 	if(arr[14][14]==0)
-	arr[14][14]=2;
+		arr[14][14]=2;
 }
-void shot(char x)
-{
-	for(int i=0;i<15;i++)
-	  for(int j=0;j<15;j++)
-	    if(arr[i][j]==1||arr[i][j]==6)
-	    {
-	    	if(x=='f'||x=='F')
-	    	{
-	    		for(int s=j;s>max(j-range,0);s--)
-	    		   if(arr[i][s]==5||arr[i][s]>=6)
-	    		   {
-	    		   	  arr[i][s]-=5;
-	    		   	  cout<<"eival";
-	    		   	  credit+=final_level+1;
-	    		   	  kill++;
-	    		   	  break;
-				   }
+void shot(char x) {
+	for(int i=0; i<15; i++)
+		for(int j=0; j<15; j++)
+			if(arr[i][j]==1||arr[i][j]==6) {
+				if(x=='f'||x=='F') {
+					for(int s=j; s>max(j-range,0); s--)
+						if(arr[i][s]==5||arr[i][s]>=6) {
+							arr[i][s]-=5;
+							cout<<"eival";
+							credit+=final_level+1;
+							kill++;
+							break;
+						}
+				} else if(x=='h'||x=='H') {
+					for(int s=j; s<min(j+range,15); s++)
+						if(arr[i][s]==5||arr[i][s]>6) {
+							arr[i][s]-=5;
+							cout<<"eival";
+							credit+=final_level+1;
+							kill++;
+							break;
+						}
+				} else if(x=='t'||x=='T') {
+					for(int s=i; s>max(0,i-range); s--)
+						if(arr[s][j]==5||arr[s][j]>6) {
+							arr[s][j]-=5;
+							cout<<"eival";
+							credit+=final_level+1;
+							kill++;
+							break;
+						}
+				} else if(x=='g'||x=='G') {
+					for(int s=i; s<min(15,i+range); s++)
+						if(arr[s][j]==5||arr[s][j]>6) {
+							arr[s][j]-=5;
+							cout<<"eival";
+							credit+=final_level+1;
+							kill++;
+							break;
+						}
+				}
 			}
-			else if(x=='h'||x=='H')
-	    	{
-	    		for(int s=j;s<min(j+range,15);s++)
-	    		   if(arr[i][s]==5||arr[i][s]>6)
-	    		   {
-	    		   	  arr[i][s]-=5;
-	    		   	  cout<<"eival";
-	    		   	  credit+=final_level+1;
-	    		   	  kill++;
-	    		   	  break;
-				   }
+}
+void zombi_Movment() {
+	int i,j,x;
+	for(int s=0; s<15; s++)
+		for(int z=0; z<15; z++)
+			if(arr[s][z]==1||arr[s][z]==6) {
+				i=s;
+				j=z;
 			}
-			else if(x=='t'||x=='T')
-	    	{
-	    		for(int s=i;s>max(0,i-range);s--)
-	    		   if(arr[s][j]==5||arr[s][j]>6)
-	    		   {
-	    		   	  arr[s][j]-=5;
-	    		   	  cout<<"eival";
-	    		   	  credit+=final_level+1;
-	    		   	  kill++;
-	    		   	  break;
-				   }
+	for(int g=0; g<15; g++)
+		for(int u=0; u<15; u++)
+			if(arr[g][u]==5||arr[g][u]>6) {
+				if(g!=i||u!=j) {
+					if(u<j) {
+						x=arr[g][u+1];
+						arr[g][u+1]=-arr[g][u+1]-5;
+					} else if(u>j) {
+						x=arr[g][u-1];
+						arr[g][u-1]=-arr[g][u-1]-5;
+					} else if(u==j) {
+						if(g<i) {
+							x=arr[g+1][u];
+							arr[g+1][u]=-arr[g+1][u]-5;
+						} else if(g>i) {
+							x=arr[g-1][u];
+							arr[g-1][u]=-arr[g-1][u]-5;
+						}
+					}
+					arr[g][u]-=5;
+				}
+				/* else
+				  {
+				arr[g][u]=6;
+				  }*/
 			}
-			else if(x=='g'||x=='G')
-	    	{
-	    		for(int s=i;s<min(15,i+range);s++)
-	    		   if(arr[s][j]==5||arr[s][j]>6)
-	    		   {
-	    		   	  arr[s][j]-=5;
-	    		   	  cout<<"eival";
-	    		   	  credit+=final_level+1;
-	    		   	  kill++;
-	    		   	  break;
-				   }
+	for(int c=0; c<15; c++)
+		for(int d=0; d<15; d++)
+			if(arr[c][d]==-5||arr[c][d]==-8||arr[c][d]==-9||arr[c][d]==-6||arr[c][d]==-10) {
+				temp=arr[c][d];
+				arr[c][d]=abs(temp);
 			}
+}
+void death() {
+	for(int i=0; i<15; i++)
+		for(int j=0; j<15; j++)
+			if(arr[i][j]==1||arr[i][j]==6) {
+				for(int s=max(0,i-1); s<min(i+2,15); s++)
+					for(int z=max(j-1,0); z<min(j+2,15); z++)
+						if(arr[s][z]==5||arr[s][z]>6) {
+							health--;
+							if (health > 0) {
+								system("cls");
+								red();
+								cout << "The zombie is eating you! You lost one of your healths!";
+								reset_color();
+								sleep(2.5);
+							} else if ( health == 0) {
+								system("cls");
+								red();
+								cout << "You Died!\n";
+								reset_color();
+								cout << "You Lose! Would you like to try again? (y : Yes | n : No)\n";
+								cin >> order[0];
+								if ( order[0] == 'n') {
+									system("cls");
+									main_menu();
+								} else if (order[0] == 'y') {
+									system("cls");
+									cout << "The game will restart in 5 seconds...";
+									sleep(5);
+									final_level = 1;
+									credit = 0 ;
+									game_round = 0;
+									kill = 0;
+									vaccineCollected = 0;
+									tir=3;
+									kheshab=0;
+									health=3 ;
+									maxBullet = 3;
+									range=5;
+									start();
+
+								}
+
+							}
+
+						}
+			}
+}
+void check_bullet(char x) {
+	if(x=='f'||x=='F'||x=='g'||x=='G'||x=='h'||x=='H'||x=='t'||x=='T') {
+		if(tir>0){
+			shot(order[0]);
+			tir--;
 		}
-}
-void zombi_Movment()
-{
-		int i,j,x;
-	for(int s=0;s<15;s++)
-	   for(int z=0;z<15;z++)
-		   	if(arr[s][z]==1||arr[s][z]==6)
-		   	{
-		   		i=s;
-		   		j=z;
-			}
-	for(int g=0;g<15;g++)
-	   for(int u=0;u<15;u++)
-	     if(arr[g][u]==5||arr[g][u]>6)
-		 {
-		    if(g!=i||u!=j)
-		    {
-		 	if(u<j)
-			{
-				x=arr[g][u+1];
-				arr[g][u+1]=-arr[g][u+1]-5;
-			}
-			else if(u>j)
-			{
-				x=arr[g][u-1];
-				arr[g][u-1]=-arr[g][u-1]-5;
-			}	
-		 	else if(u==j)
-		 	{
-		 		if(g<i)
-		 	    {
-		 	      x=arr[g+1][u];
-		 	      arr[g+1][u]=-arr[g+1][u]-5;
-			    }
-		 	    else if(g>i)
-		 	    {
-		 	     x=arr[g-1][u];
-			     arr[g-1][u]=-arr[g-1][u]-5;
-			    }
-			}
-			arr[g][u]-=5;
-		   }
-		 /* else
-	       {
-	    	arr[g][u]=6;
-		   }*/
-	    }
-	for(int c=0;c<15;c++)
-	   for(int d=0;d<15;d++)
-	     if(arr[c][d]==-5||arr[c][d]==-8||arr[c][d]==-9||arr[c][d]==-6||arr[c][d]==-10)
-	     {   
-	         temp=arr[c][d];
-	     	 arr[c][d]=abs(temp);
-		 }	
-}
-void death()
-{
-	for(int i=0;i<15;i++)
-	  for(int j=0;j<15;j++)
-	    if(arr[i][j]==1||arr[i][j]==6)
-	    {
-        	for(int s=max(0,i-1);s<min(i+2,15);s++)
-	          for(int z=max(j-1,0);z<min(j+2,15);z++)
-	            if(arr[s][z]==5||arr[s][z]>6)
-	              {
-	     	        health--;
-		          }
-	    }
-}
-void check_bullet(char x)
-{
-	if(x=='f'||x=='F'||x=='g'||x=='G'||x=='h'||x=='H'||x=='t'||x=='T')
-	{
-		if(tir>0)
-		shot(order[0]);
-		else if(tir==0)
-		cout<<"No charged ammo!";
-		tir--;
-		if(tir==-1)
-		tir++;
+		else if(tir==0){
+			system("cls");
+			cout<<"No charged ammo!";
+			sleep(2.5);
+		}
 	}
 }
 
@@ -578,40 +642,43 @@ void upgrade(char x) {
 				system("cls");
 				if (ch == '0')
 					return;
-				
+
 				else if (ch == '1') {
-					if (maxBullet == 7) 
+					if (maxBullet == 7)
 						cout << endl << "The selected item is maximum.";
-					else if (credit < maxBullet * final_level) 
+					else if (credit < maxBullet * final_level)
 						cout << endl << "Unfortunately, your credit is not enough to get this item. Please gain " << maxBullet * final_level - credit <<" more credit by playing.";
 					else {
-						credit -= maxBullet * final_level; maxBullet ++;
+						credit -= maxBullet * final_level;
+						maxBullet ++;
 						cout  << endl << "Upgrade done successfully." << endl << "Your magazine capacity is now " << maxBullet << " .";
 					}
 				}
-				
+
 				else if (ch == '2') {
 					if (range == 10)
 						cout << endl << "The selected item is maximum.";
 					else if (credit < range + final_level)
 						cout << endl << "Unfortunately, your credit is not enough to get this item. Please gain " << range + final_level - credit << " more credit by playing.";
 					else {
-						credit -= range + final_level; range ++;
+						credit -= range + final_level;
+						range ++;
 						cout << endl << "Upgrade done successfully.Your shotgun range is now " << range << " .";
 					}
 				}
 
 				else if (ch == '3') {
-					if (health == 5) 
+					if (health == 5)
 						cout << "The selected item is maximum.";
 					else if (credit < (final_level + 1) * health)
 						cout << "Unfortunately, your credit is not enough to get this item. Please gain " << (final_level+1) * health - credit << " more credit by playing.";
 					else {
-						credit -= (final_level+1) * health ; health++;
+						credit -= (final_level+1) * health ;
+						health++;
 						cout << "Additional health received successfully.Your health is now " << health << " .";
 					}
 				}
-				
+
 			}
 		}
 	}
@@ -621,12 +688,13 @@ void reload(char x) {
 	if (x == 'r' or x == 'R') {
 		game_round--;
 		system("cls");
-		if (kheshab == 0) 
+		if (kheshab == 0)
 			cout << "No Ammo!";
 		else if (tir == maxBullet)
 			cout << "The gun is already Charged";
 		else {
-			tir++; kheshab--;
+			tir++;
+			kheshab--;
 			cout << "Reloaded!";
 		}
 	}
@@ -634,11 +702,30 @@ void reload(char x) {
 
 void levelFinish() {
 	if (arr[14][14] == 1) {
-		if (vaccineCollected == final_level) {
+		if (vaccineCollected == final_level && final_level == 20) {
+			system("cls");
+			green();
+			cout << "Congratulations! You have successfully collected all the vaccines!\nNow it is time to return to the Earth\nGood Bye!";
+			sleep(2.5);
+			reset_color();
+			main_menu();
+		} else if (vaccineCollected == final_level) {
+			system("cls");
 			final_level++;
 			vaccineCollected = 0;
 			game_round=0;
+			green();
+			cout << "You Won! You have reached level " << final_level;
+			sleep(2.5);
+			reset_color();
 			start();
+		} else {
+			system("cls");
+			red();
+			cout << "First get all the vaccines.";
+			sleep(2.5);
+			reset_color();
+
 		}
 	}
 }
